600677bb89a1ef87af93322e1d2dff8d2a7b735c
Raphael Villas <raphael.villas@gmail.com>
2018-10-30 22:22:15 -0500

    Added 'cowboy' and 'plug'

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule Howitgoesbot.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:cowboy, "~>1.0.0"},
+     {:plug, "~>1.0"}]
   end
 end

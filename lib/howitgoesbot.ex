3433b8c0258c4bd62f4b647bba8f0419a7621735
Raphael Villas <raphael.villas@gmail.com>
2018-11-07 21:28:23 -0600

    Update configs

--- lib/howitgoesbot.ex
+++ lib/howitgoesbot.ex
@@ -2,7 +2,14 @@ defmodule Howitgoesbot do
   use Application
 
   def start(_type, _args) do
-    children = [ Plug.Adapters.Cowboy.child_spec(scheme: :http, plug: Howitgoesbot.Router, options: [port: 4001])]
-    Supervisor.start_link(children, strategy: :one_for_one)
+    import Supervisor.Spec, warn: false
+
+    children = [
+      Plug.Adapters.Cowboy.child_spec(scheme: :http, plug: Howitgoesbot.Router, options: [port: 4001]),
+      supervisor(Howitgoesbot.Repo, [])
+    ]
+
+    opts = [strategy: :one_for_one, name: Howitgoesbot.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
